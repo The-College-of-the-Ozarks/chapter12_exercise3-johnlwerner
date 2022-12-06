0c028448c9dfa92259c4e9258d2c4e497eb73b29
johnlwerner <97613628+johnlwerner@users.noreply.github.com>
2022-12-06 19:46:19 +0000

    completed the problem

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-class Main {
-{   
+class Main {   
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
@@ -9,11 +8,27 @@ class Main {
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
       System.out.print("Enter quarts needed >> ");
+     while (true){
+        
+     try
+      {
       quartsNeeded = input.nextInt();
+      }
+
+     catch(Exception e)
+        {
+          System.out.println("Exception occurred");
+          input.nextLine();
+          continue;
+        }
+        break;
+      }
       gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
       extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
       System.out.println("A job that needs " + quartsNeeded +
          " quarts requires " + gallonsNeeded + " gallons plus " +
          extraQuartsNeeded + " quarts.");
    }
+
+  
 }
